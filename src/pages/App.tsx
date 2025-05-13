bb7cd955c6c9b3b230b9a9fd820f93e48a8447eb
BrandonEastwell <brandoneastwell@yahoo.com>
2025-05-13 11:36:48 +0100

    html form questions

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,10 +6,52 @@ function App() {
     <>
         <form>
             <fieldset className="question active">
+                <h2>Where do you live?</h2>
 
+                <label htmlFor="option-flat">Flat</label>
+                <input type="radio" id="option-flat" name="group1" />
+
+                <label htmlFor="option-shared-house">Shared House</label>
+                <input type="radio" id="option-shared-house" name="group1" />
+
+                <label htmlFor="option-house">House</label>
+                <input type="radio" id="option-house" name="group1" />
+
+                <label htmlFor="option-with-family">With Family</label>
+                <input type="radio" id="option-with-family" name="group1" />
             </fieldset>
-            <fieldset className="question">
 
+            <fieldset className="question">
+                <h2>What is your mouse expensive belonging?</h2>
+                <label htmlFor="belonging">What is your mouse expensive belonging?</label>
+                <input type="text" id="belonging" name="group2" />
+            </fieldset>
+
+            <fieldset className="question">
+                <h2>Do you travel a lot?</h2>
+                <label htmlFor="travel-yes">Yes</label>
+                <input type="radio" id="travel-yes" name="group3" />
+
+                <label htmlFor="travel-no">No</label>
+                <input type="radio" id="travel-no" name="group3" />
+            </fieldset>
+
+            <fieldset className="question">
+                <h2>Ever had to file a claim before?</h2>
+                <label htmlFor="claim-yes">Yes</label>
+                <input type="radio" id="claim-yes" name="group4" />
+
+                <label htmlFor="claim-no">No</label>
+                <input type="radio" id="claim-no" name="group4" />
+            </fieldset>
+
+            <fieldset className="question">
+                <h2>Are you a student?</h2>
+                <label htmlFor="student-yes">Yes</label>
+                <input type="radio" id="student-yes" name="group5" />
+
+                <label htmlFor="student-no">No</label>
+                <input type="radio" id="student-no" name="group5" />
             </fieldset>
         </form>
     </>
