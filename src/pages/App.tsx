bb599fa83669460ed2f113a8d94e602c0a429a70
BrandonEastwell <brandoneastwell@yahoo.com>
2025-05-13 21:45:12 +0100

    styles & back btn

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import React, {useState} from "react";
+import svg from "../assets/urbanjungle.svg"
 
 interface FormData {
     location: null | string,
@@ -10,7 +11,6 @@ interface FormData {
 }
 
 function App() {
-    // Number of questions
     const QUESTIONS = 4;
     const [step, setStep] = useState<number>(0);
     const [formData, setFormData] = useState<FormData>({ location: null, item: null, travel: null, claim: null, student: null });
@@ -55,62 +55,79 @@ function App() {
 
     return (
         <>
-            <form onSubmit={(event) => submitHandler(event)} noValidate={true}>
-                { step === 0 && <fieldset className="question">
-                    <h2>Where do you live?</h2>
+            <div className="header">
+                <div>
+                    <img src={svg} alt="Urban Jungle logo"/>
+                </div>
+            </div>
+            <div className="content-container">
+                <h1>Find out what coverage you need.</h1>
+                <form onSubmit={(event) => submitHandler(event)} noValidate={true}>
+                    { step === 0 &&
+                        <fieldset className="question">
+                            <h2>Where do you live?</h2>
+                            <div className="radio-container">
+                                <input type="radio" id="option-flat" name="location" value="flat" onChange={formChangeHandler} required />
+                                <label htmlFor="option-flat">Flat</label>
 
-                    <div className="radio-container">
-                        <input type="radio" id="option-flat" name="location" value="flat" onChange={formChangeHandler} required />
-                        <label htmlFor="option-flat">Flat</label>
+                                <input type="radio" id="option-shared-house" name="location" value="shared house" onChange={formChangeHandler} required />
+                                <label htmlFor="option-shared-house">Shared House</label>
 
-                        <input type="radio" id="option-shared-house" name="location" value="shared house" onChange={formChangeHandler} required />
-                        <label htmlFor="option-shared-house">Shared House</label>
+                                <input type="radio" id="option-house" name="location" value="house" onChange={formChangeHandler} required />
+                                <label htmlFor="option-house">House</label>
 
-                        <input type="radio" id="option-house" name="location" value="house" onChange={formChangeHandler} required />
-                        <label htmlFor="option-house">House</label>
+                                <input type="radio" id="option-with-family" name="location" value="with family" onChange={formChangeHandler} required />
+                                <label htmlFor="option-with-family">With Family</label>
+                            </div>
+                        </fieldset> }
 
-                        <input type="radio" id="option-with-family" name="location" value="with family" onChange={formChangeHandler} required />
-                        <label htmlFor="option-with-family">With Family</label>
-                    </div>
+                    { step === 1 &&
+                        <fieldset className="question">
+                            <h2>What is your most expensive belonging?</h2>
+                            <div className="text-container">
+                                <input type="text" id="belonging" name="item" value={formData.item ? formData.item : ""} onChange={formChangeHandler} required max={15} min={2} />
+                                <label htmlFor="belonging">What is your most expensive belonging?</label>
+                            </div>
+                        </fieldset> }
 
-                </fieldset> }
+                    { step === 2 &&
+                        <fieldset className="question">
+                            <h2>Do you travel a lot?</h2>
+                            <div className="radio-container">
+                                <input type="radio" id="travel-yes" name="travel" onChange={formChangeHandler} required />
+                                <label htmlFor="travel-yes">Yes</label>
+                                <input type="radio" id="travel-no" name="travel" onChange={formChangeHandler} required />
+                                <label htmlFor="travel-no">No</label>
+                            </div>
+                        </fieldset> }
 
-                { step === 1 &&
-                    <fieldset className="question">
-                        <h2>What is your most expensive belonging?</h2>
-                        <label style={{display:"none"}} htmlFor="belonging">What is your most expensive belonging?</label>
-                        <input type="text" id="belonging" name="item" onChange={formChangeHandler} required max={15} min={2} />
-                    </fieldset> }
+                    { step === 3 &&
+                        <fieldset className="question">
+                            <h2>Ever had to file a claim before?</h2>
+                            <div className="radio-container">
+                                <input type="radio" id="claim-yes" name="claim" onChange={formChangeHandler} required />
+                                <label htmlFor="claim-yes">Yes</label>
+                                <input type="radio" id="claim-no" name="claim" onChange={formChangeHandler} required />
+                                <label htmlFor="claim-no">No</label>
+                            </div>
+                        </fieldset> }
 
-                { step === 2 &&
-                    <fieldset className="question">
-                        <h2>Do you travel a lot?</h2>
-                        <input type="radio" id="travel-yes" name="travel" />
-                        <label htmlFor="travel-yes">Yes</label>
-                        <input type="radio" id="travel-no" name="travel" />
-                        <label htmlFor="travel-no">No</label>
-                    </fieldset> }
+                    { step === 4 &&
+                        <fieldset className="question">
+                            <h2>Are you a student?</h2>
+                            <div className="radio-container">
+                                <input type="radio" id="student-yes" name="student" onChange={formChangeHandler} required />
+                                <label htmlFor="student-yes">Yes</label>
+                                <input type="radio" id="student-no" name="student" onChange={formChangeHandler} required />
+                                <label htmlFor="student-no">No</label>
+                            </div>
+                        </fieldset> }
 
-                { step === 3 &&
-                    <fieldset className="question">
-                        <h2>Ever had to file a claim before?</h2>
-                        <input type="radio" id="claim-yes" name="claim" />
-                        <label htmlFor="claim-yes">Yes</label>
-                        <input type="radio" id="claim-no" name="claim" />
-                        <label htmlFor="claim-no">No</label>
-                    </fieldset> }
-
-                { step === 4 &&
-                    <fieldset className="question">
-                        <h2>Are you a student?</h2>
-                        <input type="radio" id="student-yes" name="student" />
-                        <label htmlFor="student-yes">Yes</label>
-                        <input type="radio" id="student-no" name="student" />
-                        <label htmlFor="student-no">No</label>
-                    </fieldset> }
-                { formError && <span className="error">{formError}</span> }
-                <button type="submit" disabled={submitDisabled}>{step === QUESTIONS ? "Submit" : "Continue"}</button>
-            </form>
+                    { formError && <span className="error">{formError}</span> }
+                    <button type="submit" disabled={submitDisabled}>{step === QUESTIONS ? "Submit" : "Continue"}</button>
+                    { step > 0 && <a className="back-btn" onClick={() => setStep(step - 1)}>back</a> }
+                </form>
+            </div>
         </>
     )
 }
